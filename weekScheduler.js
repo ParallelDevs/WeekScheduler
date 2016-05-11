d4da8fced0e99711d7b8f93a30fe324d7af0cfc2
kvcordero <k.villanueva@paralleldevs.com>
2016-05-11 13:46:18 -0600

    Add responsive behavior.

--- weekScheduler.js
+++ weekScheduler.js
@@ -7,6 +7,7 @@
     this.render();
     this.attachEvents();
     this.$selectingStart = null;
+    this.mobile = null;
     this.firstDayWeek = null;
     this.lastDayWeek = null;
   }
@@ -37,9 +38,9 @@
    */
   WeekScheduler.prototype.render = function () {
     this.$el.html(this.options.template);
-    this.setActualWeekDays()
+    this.setActualWeekDays();
     this.renderWeekSelector();
-    this.renderHeader();
+    if (!this.mobile) { this.renderHeader(); }
     this.renderRows();
   };
 
@@ -51,11 +52,19 @@
     var firstday = this.firstDayWeek;
     var lastday = this.lastDayWeek;
 
-    var html = '<div class="week-selector-text">' + firstday.getDate() + " " + firstday.getMonthName() + " - "
-      + lastday.getDate() + " " + lastday.getMonthName() + '</div>';
+    // html in mobile
+    if (this.mobile) {
+      var html = '<div class="week-selector-text">' + this.options.stringDays[firstday.getDay()] + " "
+        + firstday.getDate() + " " + firstday.getMonthName() + '</div>';
+    }
+    // html in desktop
+    else{
+      var html = '<div class="week-selector-text">' + firstday.getDate() + " " + firstday.getMonthName() + " - "
+        + lastday.getDate() + " " + lastday.getMonthName() + '</div>';
+    }
 
-    this.$el.find('.week-selector').html('<div class="left-arrow arrow"> < </div> '
-      + html + ' <div class="right-arrow arrow"> > </div>');
+    this.$el.find('.week-selector').html('<div class="left arrow"> < </div> '
+      + html + ' <div class="right arrow"> > </div>');
   };
 
   /**
@@ -84,15 +93,16 @@
       blokedDays = this.options.blokedDays,
       days = this.options.days,
       $el = this.$el.find('.schedule-rows'),
-      firstDayWeek = this.firstDayWeek;
-
-    var a = generateDates(start, end, interval);
+      firstDayWeek = this.firstDayWeek,
+      mobile = this.mobile;
 
     $.each(generateDates(start, end, interval), function (i, d) {
 
       var daysInARow = '';
+      // if we are in mobile the for only runs 1 time.
+      var endFor = (mobile) ? 1 : days.length;
 
-      for (var j = 0; j < days.length; j++) {
+      for (var j = 0; j < endFor; j++) {
         var hour = hhmm(d);
         hour = hour.split(":");
         var dateAux = new Date(firstDayWeek.getTime() + j * 24 * 60 * 60 * 1000);
@@ -106,13 +116,14 @@
     });
   };
 
+
   //=================================================================================================================
   //                                            Functions
   //=================================================================================================================
 
 
   /**
-   * Is the day schedule selector in selecting mode?
+   * Set the actual week
    * @public
    */
   WeekScheduler.prototype.setActualWeekDays = function () {
@@ -120,15 +131,24 @@
     var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
     this.firstDayWeek = new Date(curr.setDate(first));
     this.lastDayWeek = new Date(this.firstDayWeek.getTime() + 6 * 24 * 60 * 60 * 1000);
+    this.mobile = (screen.width <= 600) ? true : false;
   }
 
   /**
-   * Is the day schedule selector in selecting mode?
+   * Update the actual week
    * @public
    */
   WeekScheduler.prototype.updateActualWeekDays = function (firstDay, lastDay) {
     this.firstDayWeek = firstDay;
     this.lastDayWeek = lastDay;
+  }
+
+  /**
+   * Update the mobile value
+   * @public
+   */
+  WeekScheduler.prototype.updateMobile = function (mobile) {
+    this.mobile = mobile;
   }
 
   /**
@@ -155,15 +175,32 @@
    * Update the timestamp in all timeslot
    * @public
    */
-  WeekScheduler.prototype.updateTimeslot = function (arrow) {
+  WeekScheduler.prototype.updateTimeslot = function (right, mobile) {
     this.$el.find(".time-slot").each(function (index) {
 
       var timestamp = parseInt($(this).data("timestamp"));
 
-      if (arrow === "left") {
-        timestamp -= 7 * 24 * 60 * 60 * 1000
-      } else {
-        timestamp += 7 * 24 * 60 * 60 * 1000
+      // update the timestamp to the next day or week
+      if (right) {
+        // update one day
+        if(mobile){
+          timestamp += 1 * 24 * 60 * 60 * 1000;
+        }
+        // update one week
+        else{
+          timestamp += 7 * 24 * 60 * 60 * 1000;
+        }
+      }
+      // update the timestamp to the last day or week
+      else {
+        // update one day
+        if(mobile){
+          timestamp -= 1 * 24 * 60 * 60 * 1000;
+        }
+        // update one week
+        else{
+          timestamp -= 7 * 24 * 60 * 60 * 1000;
+        }
       }
 
       $(this).data("timestamp", timestamp);
@@ -176,12 +213,28 @@
   //=================================================================================================================
   //                                            Manage Events
   //=================================================================================================================
-
   WeekScheduler.prototype.attachEvents = function () {
     var plugin = this,
-      options = this.options,
       firstDayWeek = this.firstDayWeek,
-      $slots;
+      mobile = this.mobile;
+
+    // Window resize
+    //=================================
+    $( window ).on( "resize", function( event ) {
+
+      if(screen.width <= 600 && !mobile){
+        $("#day-schedule").html("");
+        mobile = true;
+        plugin.render();
+      }else{
+        if(screen.width > 600 && mobile){
+          $("#day-schedule").html("");
+          mobile = false;
+          plugin.render();
+        }
+      }
+
+    });
 
     //  Click on time slot
     //=================================
@@ -202,37 +255,49 @@
       }
     });
 
-    //  Click left arrow
+    //  Click left/right arrow
     //=================================
-    this.$el.on('click', '.left-arrow', function () {
-      plugin.firstDayWeek = new Date(firstDayWeek.getTime() - 7 * 24 * 60 * 60 * 1000);
-      var lastDayWeek = new Date(plugin.firstDayWeek.getTime() + 6 * 24 * 60 * 60 * 1000);
+    this.$el.on('click', '.arrow', function () {
 
-      plugin.$el.find('.week-selector-text').html(plugin.firstDayWeek.getDate()
-        + " " + plugin.firstDayWeek.getMonthName() + " - " + lastDayWeek.getDate()
-        + " " + lastDayWeek.getMonthName());
+      var right = $(this).hasClass( "right" )
+
+      // move the day in mobile
+      if(mobile){
+
+        // add day if click in right arrow
+        if(right){
+          plugin.firstDayWeek = new Date(firstDayWeek.getTime() + 1 * 24 * 60 * 60 * 1000);
+        }
+        // subtract day if click in left arrow
+        else{
+          plugin.firstDayWeek = new Date(firstDayWeek.getTime() - 1 * 24 * 60 * 60 * 1000);
+        }
+
+        plugin.$el.find('.week-selector-text').html(plugin.options.stringDays[plugin.firstDayWeek.getDay()] + " "
+          + plugin.firstDayWeek.getDate() + " " + plugin.firstDayWeek.getMonthName());
+      }
+      //move the week in desktop
+      else{
+        // add day if click in right arrow
+        if(right){
+          plugin.firstDayWeek = new Date(firstDayWeek.getTime() + 7 * 24 * 60 * 60 * 1000);
+          var lastDayWeek = new Date(plugin.firstDayWeek.getTime() + 6 * 24 * 60 * 60 * 1000);
+        }
+        // subtract day if click in left arrow
+        else{
+          plugin.firstDayWeek = new Date(firstDayWeek.getTime() - 7 * 24 * 60 * 60 * 1000);
+          var lastDayWeek = new Date(plugin.firstDayWeek.getTime() + 6 * 24 * 60 * 60 * 1000);
+        }
+
+        plugin.$el.find('.week-selector-text').html(plugin.firstDayWeek.getDate()
+          + " " + plugin.firstDayWeek.getMonthName() + " - " + lastDayWeek.getDate()
+          + " " + lastDayWeek.getMonthName());
+      }
 
       plugin.updateActualWeekDays(plugin.firstDayWeek, lastDayWeek);
       firstDayWeek = plugin.firstDayWeek;
-      plugin.updateTimeslot("left");
+      plugin.updateTimeslot(right, mobile);
     });
-
-    //  Click right arrow
-    //=================================
-    this.$el.on('click', '.right-arrow', function () {
-
-      plugin.firstDayWeek = new Date(firstDayWeek.getTime() + 7 * 24 * 60 * 60 * 1000);
-      var lastDayWeek = new Date(plugin.firstDayWeek.getTime() + 6 * 24 * 60 * 60 * 1000);
-
-      plugin.$el.find('.week-selector-text').html(plugin.firstDayWeek.getDate()
-        + " " + plugin.firstDayWeek.getMonthName() + " - " + lastDayWeek.getDate()
-        + " " + lastDayWeek.getMonthName());
-
-      plugin.updateActualWeekDays(plugin.firstDayWeek, lastDayWeek);
-      firstDayWeek = plugin.firstDayWeek;
-      plugin.updateTimeslot("right");
-    });
-
 
   };
 
@@ -279,7 +344,7 @@
    * @private
    */
   function timeDiff(start, end) {   // time in HH:mm format
-    // need a dummy date to utilize the Date object
+                                    // need a dummy date to utilize the Date object
     return (new Date(2000, 0, 1, end.split(':')[0], end.split(':')[1]).getTime() -
       new Date(2000, 0, 1, start.split(':')[0], start.split(':')[1]).getTime()) / 60000;
   }
